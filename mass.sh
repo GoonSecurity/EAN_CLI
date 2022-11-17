65e7221a6d435dcffd5c814beac979df06a3342e
chakal1337 <xorrsprsp@gmail.com>
2022-11-18 00:24:30 +0100

    yes

---
+++ mass.sh
@@ -0,0 +1,7 @@
+if [ "$#" -lt 1 ]; then
+ echo "$0 <domain>";
+ exit;
+fi;
+echo "Starting...";
+rm /tmp/tmpeanoutput.txt;
+assetfinder -subs-only $1 | xargs -L 1 echo https:// | tr -d " " | xargs -L 1 python3 ./tokens.py -t $1 -u | tee -a /tmp/tmpeanoutput.txt;
--- tokens.py
+++ tokens.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import re
 import requests
 import argparse
@@ -11,24 +12,27 @@ import analysis
 
 #parse command line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--tld')
 parser.add_argument('-u', '--url')
 parser.add_argument('-m', '--max', default=False)
 args, domains = parser.parse_known_args()
 
 
 url = args.url
+tld = args.tld
 maximum_tokens = args.max
 
 
 def get_all_scripts(url):
 	try:
-		source = requests.get(url, allow_redirects=True, timeout=3).text
+		source = requests.get(url, allow_redirects=True, timeout=5).text
+		#print(source)
 	except:
 		return []
 
 
 	soup = BeautifulSoup(source, "html.parser")
-
+	#print(soup)
 
 	all_script_links = {}
 	initial_script_links = []
@@ -36,6 +40,7 @@ def get_all_scripts(url):
 	#get script links in homepage
 	for script in soup.find_all("script"):
 	    if script.attrs.get("src"):
+	        #print(script.attrs.get("src"))
 
 	        #if the tag has the attribute 'src'
 	        script_url = urljoin(url, script.attrs.get("src"))
@@ -45,9 +50,10 @@ def get_all_scripts(url):
 
 	#search found scripts for more javascript files
 	for script_link in initial_script_links:
+		#print(script_link)
 
 		try:
-			content = requests.get(script_link, timeout=3).text
+			content = requests.get(script_link, timeout=5).text
 		except:
 			continue
 
@@ -75,11 +81,13 @@ def get_all_scripts(url):
 def get_all_tokens(url):
 	all_tokens = []
 	token_data = []
-
+	print("Starting... {}".format(url))
 	#parse script files for tokens
 	script_links = get_all_scripts(url)
 
 	for script_link in script_links:
+		if not tld in script_link: continue
+		print(script_link)
 
 		#get all script data
 		script_data = script_links[script_link]
@@ -104,11 +112,7 @@ def get_all_tokens(url):
 
 token_data = get_all_tokens(url)
 
-loop = 0
-for data in token_data:
-	loop += 1
-	if loop == 1: continue
-	
+for data in token_data:	
 
 	token = data['token']
 	url   = data['url']
